28d257ef83438c281b38873c029c789280fef5ab
Jared McInerney <JJMC106@gmail.com>
2017-11-21 19:10:57 +0000

    Added more code to BackStoryWindow. Allowing User to select a pre-set background

--- Text_ADV/src/CharectarCreationGUI.java
+++ Text_ADV/src/CharectarCreationGUI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class CharectarCreationGUI extends JFrame  {
@@ -9,7 +11,7 @@ public class CharectarCreationGUI extends JFrame  {
            JFrame CharectarWindow = new JFrame("Create Your Adventurer");
            GridLayout layout = new GridLayout(0,2);
            CharectarWindow.setLayout(layout);
-           CharectarWindow.setSize(350,150);
+           CharectarWindow.setSize(350,180);
            CharectarWindow.setVisible(true);
            CharectarWindow.getContentPane().setBackground(Color.BLACK);
 
@@ -46,6 +48,32 @@ public class CharectarCreationGUI extends JFrame  {
            CharectarWindow.add(VulnerabilityInput);
 
 
+           JButton Confirm = new JButton("Create");
+           CharectarWindow.add(Confirm);
+           Confirm.setBackground(Color.gray);
+
+           Confirm.addActionListener(new ActionListener() {
+               @Override
+               public void actionPerformed(ActionEvent e) {
+                   JFrame BackStoryWindow = new JFrame("Whats your story?");
+                   GridLayout layoutB = new GridLayout(0,3);
+                   BackStoryWindow.setLayout(layoutB);
+                   BackStoryWindow.setSize(500,200);
+                   BackStoryWindow.setVisible(true);
+
+                   JButton Thief = new JButton("Lonely Thief");
+                   BackStoryWindow.add(Thief);
+                   Thief.setBackground(Color.gray);
+
+                   JButton Farmer = new JButton("Lost Farmer");
+                   BackStoryWindow.add(Farmer);
+                   Farmer.setBackground(Color.gray);
+
+                   JButton Soldier = new JButton("Wandering Mercenary");
+                    BackStoryWindow.add(Soldier);
+                   Soldier.setBackground(Color.gray);
+               }
+           });
 
        }}
 
